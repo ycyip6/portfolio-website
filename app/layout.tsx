d7171caca685a8562e12816d57fd4f987119557d
HoiPang2 <hoipang1e06@gmail.com>
2024-03-14 12:00:33 -0400

    fix: update meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Personal Portfolio",
+  description: "Powered by ByteGrad",
 };
 
 export default function RootLayout({
