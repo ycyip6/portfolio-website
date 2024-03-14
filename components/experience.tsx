2e134adbf7f16ee3dceedfe6985ae173b9d91884
HoiPang2 <hoipang1e06@gmail.com>
2024-03-14 12:49:04 -0400

    feat: update experiences section

--- components/experience.tsx
+++ components/experience.tsx
@@ -45,7 +45,13 @@ export default function Experience() {
               }}
             >
               <h3 className="font-semibold capitalize">{item.title}</h3>
-              <p className="font-normal !mt-0">{item.location}</p>
+              <a
+                className="font-normal !mt-0 text-blue-600"
+                href={item.institutionLink}
+                target="_blank"
+              >
+                {item.institution}
+              </a>
               <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {item.description}
               </p>
