45fab061d7debd4252948a88394b8223b228e56f
HoiPang2 <hoipang1e06@gmail.com>
2024-03-16 11:52:38 -0400

    feat: add qualifications section

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,17 @@ import About from "@/components/about";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
+import { Qualifications } from "@/components/qualifications";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4">
+    <main className="flex flex-col items-center px-4 overflow-x-hidden">
       <Intro />
       <SectionDivider />
       <About />
+      <Qualifications />
       <Projects />
       <Skills />
       <Experience />
